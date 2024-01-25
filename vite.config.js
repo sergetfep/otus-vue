c29391cf8c9c01253cb394df18d05111b2fbbf1e
Serge <nsecuritysg@nxt.ru>
2024-01-25 19:16:07 +0300

    add Eslint

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
